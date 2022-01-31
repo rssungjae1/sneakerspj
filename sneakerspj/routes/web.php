63fe070cc5813493671c3e811f3e6aeb149d5a08
RonieDevStitch <rssungjae1@gmail.com>
2022-01-31 19:07:18 +0900

    [add] route設定
    indexページ
    商品詳細ページ
    カートページ
    カートに入れるボタン
    ajax

--- sneakerspj/routes/web.php
+++ sneakerspj/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// indexページ
+Route::get('/', [ProductsController::class, 'index']);
+// 商品詳細ページ
+Route::get('product/{id}', [ProductsController::class, 'show'])->name('show');
+// カートページ
+Route::get('cart', [ProductsController::class, 'cart'])->name('cart');
+// カートに入れるボタン
+Route::get('add-cart/{id}', [ProductsController::class, 'addCart'])->name('add.cart');
+// ajax
+Route::patch('update-cart', [ProductsController::class, 'update'])->name('update.cart');
+Route::delete('remove-from-cart', [ProductsController::class, 'remove'])->name('remove.from.cart');
